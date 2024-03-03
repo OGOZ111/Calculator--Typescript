ffebefb00af85776fd6ec38d13231e042103626f
OGOZ111 <142310217+OGOZ111@users.noreply.github.com>
2024-03-03 17:21:35 +0200

    added usestates for operation, AC buton. fixed small MUI issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Paper, styled, Container, Grid } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { GridOperationButton } from "./GridOperationButton";
 
 const OutputContainer = styled("div")(({ theme }) => ({
   width: "100%",
@@ -17,12 +18,29 @@ const CalcualatorBase = styled(Paper)(({ theme }) => ({
 }));
 
 function App() {
+  const [currentValue, setCurrentValue] = useState("0");
+  const [operation, setOperation] = useState("");
+
+  const selectOpperation = (operation: string) => {
+    setOperation(operation);
+  };
+
+  const setDigit = (digit: string) => {
+    setCurrentValue(digit);
+  };
   return (
     <Container maxWidth="sm">
       <CalcualatorBase elevation={3}>
         <Grid container spacing={1}>
           <Grid item xs={12}>
-            <OutputContainer>0</OutputContainer>
+            <OutputContainer>{currentValue}</OutputContainer>
+          </Grid>
+          <Grid item columnSpacing={1}>
+            <GridOperationButton
+              operation="AC"
+              selectOperation={selectOpperation}
+              selectedOperation={operation}
+            />
           </Grid>
         </Grid>
       </CalcualatorBase>
