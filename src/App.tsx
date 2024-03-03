9ad3ba0ad60bc83bc1efc165d91f7448b0fe6c10
OGOZ111 <142310217+OGOZ111@users.noreply.github.com>
2024-03-03 16:36:16 +0200

    defined container for number display, MUI. Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { Paper, styled, Container, Grid } from "@mui/material";
 import React from "react";
 
+const OutputContainer = styled("div")(({ theme }) => ({
+  width: "100%",
+  textAlign: "right",
+  height: "2em",
+  padding: theme.spacing(2),
+  fontSize: "3em",
+  overflow: "hidden",
+}));
+
+const CalcualatorBase = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginTop: theme.spacing(4),
+  borderRadius: 15,
+}));
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container maxWidth="sm">
+      <CalcualatorBase elevation={3}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <OutputContainer>0</OutputContainer>
+          </Grid>
+        </Grid>
+      </CalcualatorBase>
+    </Container>
+  );
 }
 
 export default App;
